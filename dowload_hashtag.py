343a3b9b7dafb35ecfd4961efeae57bc92aa4b41
Mario <mquintanaa.ia@gmail.com>
2016-12-29 19:37:16 +0100

    Script to download updated with ftp dir

--- dowload_hashtag.py
+++ dowload_hashtag.py
@@ -1,22 +1,19 @@
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-import random
 import re
 from selenium import webdriver;
 import time
 import urllib;
 import os;
-import random;
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
-import sys
 import traceback
 
 
 ######################GLOBAL VARS################################
 caps = DesiredCapabilities.FIREFOX
 exp = r'https:\/\/scontent-mad1[a-z|A-Z|_|\/|\-|\.|0-9|+\.]+'
-TIME_TO_WAIT = 300
+TIME_TO_WAIT = 60
+FTP_DIR = '/tmp/' #put slash at the end of the var. The photos will be download in this dir + hashtag
 ################################################################
 
 def is_element_present(driver, how, what):
@@ -32,50 +29,47 @@ def hay_nuevas_imagenes(encont):
         return True
     else:
         return False
-def descargarImagenes(html,hashtag,links, point):
-    i = point
-    for link in links[point:]:
-        direc = os.getcwd() + '/' + hashtag + '/' + re.findall(r'\w+.jpg', link)[0]
+def descargarImagenes(hashtag,links):
+    i = 0
+    for link in links:
+        direc = FTP_DIR + hashtag + '/' + re.findall(r'\w+.jpg', link)[0]
         if not os.path.isfile(direc):
             print "Downloaded ", direc, 'number', i
             urllib.urlretrieve(link, direc)
             i += 1
         else:
             print direc + " already exist"
-    return i
+
 def wait_and_quit(driver):
     driver.delete_all_cookies()
-    #driver.quit()
     print "All done. I am going to sleep", TIME_TO_WAIT, "seconds"
     time.sleep(TIME_TO_WAIT)
     driver.get("https://www.instagram.com/explore/tags/" + hashtag)
     time.sleep(5)
     driver.find_element_by_class_name('_oidfu').click()
-    html = (driver.page_source).encode('ascii', 'ignore')
-    return re.findall(exp, html)
 
-hashtag = 'cuadrilla5' #raw_input('Put the hashtag to be downloaded: \n')
-if not os.path.exists('./'+hashtag): # Directory that I want to save the image to
+
+hashtag = raw_input('Put the hashtag to be downloaded: \n')
+if not os.path.exists(FTP_DIR + hashtag): # Directory that I want to save the image to
         os.mkdir(hashtag) # If no directory create it
-#hashtag = 'justlikethis'
 
 caps["marionette"] = True
-driver = webdriver.Firefox()  #capabilities=caps)
+driver = webdriver.Firefox(capabilities=caps)
 driver.get("https://www.instagram.com/explore/tags/" + hashtag)
-#import ipdb; ipdb.set_trace(context=5)
 time.sleep(5)
 driver.find_element_by_class_name('_oidfu').click()
-html = (driver.page_source).encode('ascii', 'ignore')
-links = re.findall(exp, html)
-point = 0
-import ipdb; ipdb.set_trace(context=5)
 while True:
-    while hay_nuevas_imagenes(links):
-        point = descargarImagenes(html, hashtag, links, point)
+    ant = driver.execute_script('return window.pageYOffset;')
+    dsp = 0
+    while ant != dsp:
+        ant = dsp
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(2)
-        html = (driver.page_source).encode('ascii', 'ignore')
-        links = re.findall(exp, html)
-    enc = wait_and_quit(driver)
+        dsp = driver.execute_script('return window.pageYOffset;')
+        print ant,  dsp
+    html = (driver.page_source).encode('ascii', 'ignore')
+    links = re.findall(exp, html)
+    descargarImagenes(hashtag, links)
+    wait_and_quit(driver)
 
 
